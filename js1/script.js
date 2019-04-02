553390b683b419b0f7f564598b594016555f0b5f
asolana <a8alumno@aules.local>
2019-04-02 17:37:58 +0200

    Not bad

---
+++ js1/script.js
@@ -0,0 +1,22 @@
+let sec = document.getElementById('sect');
+sec.style.background = "#00d9ff";
+
+//Assignacion asincrona
+
+sec.onclick = ()=>{
+    dihola("Aitor");
+}
+
+//Funcion asignada a una variable/constante
+
+const dihola = function (x) {
+    alert('Hola ' + x);
+}
+
+
+//Funcion anonima auto invocada
+(function (x){
+    sec.onclick = function() {
+        dihola(x);
+    }
+})("Aitor");
